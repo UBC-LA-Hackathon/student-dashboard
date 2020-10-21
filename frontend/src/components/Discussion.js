208cdea79172cfb49156d702b1f25a8d3b862783
Marko Prodanovic <marko.prodanovic@outlook.com>
2020-10-21 10:53:40 -0700

    Solution for step 3

--- frontend/src/components/Discussion.js
+++ frontend/src/components/Discussion.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import Heatmap from './Heatmap'
 
-function Discussion () {
+function Discussion() {
   const [discussion, setDiscussion] = useState([])
   const [timestamps, setTimestamps] = useState([])
 
   // add useEffect here for discussion
+  useEffect(() => {
+    fetch('http://localhost:4001/getDiscussions')
+      .then(res => res.json())
+      .then(data => setDiscussion(data))
+  }, [])
 
   useEffect(() => {
     if (discussion.length > 0) {
--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 
-function Welcome () {
+function Welcome() {
   const [self, setSelf] = useState('attendee')
 
   // add useEffect here for self
+  useEffect(() => {
+    fetch('http://localhost:4001/getSelf')
+      .then(res => res.json())
+      .then(data => setSelf(data.name))
+  }, [])
 
   return (
     <p>Welcome to the Workshopathon, {self}!</p>
