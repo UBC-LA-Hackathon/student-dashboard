b237d10aa48731caa16e5caaca878231f6fec894
Marko Prodanovic <marko.prodanovic@outlook.com>
2020-10-21 13:11:47 -0700

    Solution for step 5

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,14 @@ app.get('/getSelf', async (req, res) => {
 })
 
 // Make endpoint for getDiscussions here
-app.get('/getDiscussions', async (req, res) => {
+app.get('/getDiscussions/:id', async (req, res) => {
 
-    // call getDiscussions with the Canvas course id of our choice
+    // get the course id from our request parameters
+    const courseId = req.params.id
+
+    // call getDiscussions with the Canvas course id passed in
     // await the response
-    const response = await getDiscussions(69306)
+    const response = await getDiscussions(courseId)
 
     // call flattenTopicAndReplies on the response
     const discussions = flattenTopicAndReplies(response)
@@ -37,5 +40,26 @@ app.get('/getDiscussions', async (req, res) => {
     res.json(discussions)
 })
 
+app.get('/getCourses', async (req, res) => {
+
+    // The Try/Catch syntax is a way of handling runtime errors in JavaScript
+    // Read more about it here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+    // This isn't required for your web app to work, but it gives us more control over how errors are handled
+
+    try {
+        // get our Canvas user data from the Canvas API
+        const self = await canvasAPI.getSelf()
+
+        // make another call to Canvas API for our courses and pass in our user id
+        const courses = await canvasAPI.getCoursesByUser(self.id)
+
+        // serve the response on the endpoint as JSON data
+        res.json(courses)
+    } catch {
+
+        // if something goes wrong in the Try block, we'll send this message instead of data
+        res.send('An error has occured!')
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
