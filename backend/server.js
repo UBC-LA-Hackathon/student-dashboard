4b7481692d8dc8ff439bcf577be9a7ab0402c424
Marko Prodanovic <marko.prodanovic@outlook.com>
2020-10-21 00:19:38 -0700

    Solution for step 2

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,26 @@ canvasAPI.getSelf()
 
 
 // Make endpoint for getSelf here
-
+app.get('/getSelf', async (req, res) => {
+    // we need to now make a call to the Canvas API,
+    // wait for the response, then send the result to the frontend
+    const self = await canvasAPI.getSelf()
+    res.json(self)
+})
 
 // Make endpoint for getDiscussions here
+app.get('/getDiscussions', async (req, res) => {
+
+    // call getDiscussions with the Canvas course id of our choice
+    // await the response
+    const response = await getDiscussions(69306)
+
+    // call flattenTopicAndReplies on the response
+    const discussions = flattenTopicAndReplies(response)
+
+    // send response back to frontend
+    res.json(discussions)
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
